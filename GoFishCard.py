76c2a1caf88783bcb5367476e72824ec22aa70db
Ncollazo <collazodnatalia@outlook.com>
2024-01-08 20:43:51 -0500

    update GoFishCard to add number to the numders

--- GoFishCard.py
+++ GoFishCard.py
@@ -12,24 +12,19 @@ class GoFishCard:
     # Creates a single Card with a random suit  and numder
     # @param cardSuits, is the four allowed  
     # @param cardNum, is the 13 allowed  
-    def __init__ (self, cardSuits, cardNum):
+    def __init__(self, cardSuits, cardNum):
+        num_to_int = {
+            "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6,
+            "seven": 7, "eight": 8, "nine": 9, "ten": 10, "jack": 11, "queen": 12, "king": 13
+        }
         
-        #card numder   
-        numder = ["one","two","three","four", "five","six","seven","eight","nine","ten","jack", "queen"," king"]
+        suits = ["spades", "hearts", "diamonds", "clubs"]
+        assert cardSuits.lower() in suits, "Error: Suit should be one of the four types."
         
-        # card suits 
-        suits = ["spades","hearts","diamonds","clubs"]
+        assert cardNum.lower() in num_to_int.keys(), "Error: Card number should be one of the thirteen types."
         
-          
-        assert cardSuits.lower() in suits ,"Error: only suits can be only theses 4 types"
-        
-
-
-        assert cardNum.lower() in numder , "Error: only num it can only be theses 13 numders"
-        
-        self._numder = cardNum
-        
-        self._suits = cardSuits
+        self._numder = num_to_int[cardNum.lower()]
+        self._suits = cardSuits.lower()
       
     # number of the card as a string 
     #@return  the card num as a string in lowercase.
@@ -52,7 +47,7 @@ class GoFishCard:
     
     #string representation of the card of the form capitalized first letter of color and capitalized breed
     def __repr__(self):
-        cardSuits1 = self._suits[0].upper()
-        cardNum = self._numder.upper()
-        return cardSuits1 + ":" + cardNum
+        card_suits = self._suits[0].upper()
+        card_num = str(self._numder).upper()
+        return card_suits + ":" + card_num
         
